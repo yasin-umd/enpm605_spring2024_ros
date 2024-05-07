db2ec99fce1e532cf437414b16f9ed822f291299
zeidk <zeidk@umd.edu>
2024-05-07 10:01:35 -0400

    Python code for navigation

---
+++ navigation_demo/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'navigation_demo'
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
+        (os.path.join("share", package_name, "map"), glob("map/*")),
+        (os.path.join("share", package_name, "world"), glob("world/*.world")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="zeid",
+    maintainer_email="zeidk@umd.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
