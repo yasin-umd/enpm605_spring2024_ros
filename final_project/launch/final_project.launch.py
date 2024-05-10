e953a4529bb6b58bd3e6883acf4e34fc91ed4848
zeidk <zeidk@umd.edu>
2024-05-09 21:58:28 -0400

    moved final project

---
+++ final_project/launch/final_project.launch.py
@@ -0,0 +1,132 @@
+import os
+import yaml
+import rclpy.logging
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
+
+from ament_index_python.packages import (
+    get_package_share_directory,
+    PackageNotFoundError,
+)
+
+
+def launch_setup(context, *args, **kwargs):
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+
+    # Set the path to this package.
+    final_project_pkg = FindPackageShare(package="final_project").find("final_project")
+    final_project_launch_dir = os.path.join(final_project_pkg, "launch")
+
+    # Set the path to the sensor files
+    user_config_path = os.path.join(final_project_pkg, "config", "sensors.yaml")
+
+    if not os.path.exists(user_config_path):
+        rclpy.logging.get_logger("Launch File").fatal(
+            f"Sensor configuration 'sensors.yaml' not found in config directory: {user_config_path}."
+        )
+        exit()
+
+    # Sensor TF
+    sensor_broadcaster_cmd = Node(
+        package="final_project",
+        executable="sensor_tf_broadcaster.py",
+        output="screen",
+        arguments=[user_config_path],
+        parameters=[
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    # Environment Startup
+    environment_startup_cmd = Node(
+        package="final_project",
+        executable="environment_startup_node.py",
+        output="screen",
+        parameters=[
+            {"user_config_path": user_config_path},
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    # Part spawner
+    part_spawner_cmd = Node(
+        package="final_project", executable="part_spawner.py", output="screen"
+    )
+
+    # Aruco detection
+    aruco_detection_cmd = Node(
+        package="ros2_aruco", executable="aruco_node", output="screen"
+    )
+
+    static_transform_cmd = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=[
+            "0",  # x
+            "0",  # y
+            "0",  # z
+            "0",  # r
+            "0",  # p
+            "0",  # y
+            "world",  # frame_id
+            "leader/map",  # child_frame_id
+        ],
+    )
+
+    # Start multi robots
+    multi_robots_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(final_project_launch_dir, "multi_robots.launch.py")
+        ),
+    )
+
+    # Nodes to start
+    nodes_to_start = [
+        environment_startup_cmd,
+        part_spawner_cmd,
+        sensor_broadcaster_cmd,
+        # static_transform_cmd,
+        multi_robots_cmd,
+        aruco_detection_cmd,
+    ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sensor_config",
+            default_value="sensors",
+            description="name of user configuration file",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "rviz",
+            default_value="false",
+            description="Whether to start RViz",
+        )
+    )
+
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
---
+++ final_project/launch/spawn_turtlebot3.launch.py
@@ -0,0 +1,84 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the urdf file
+   
+
+    # Launch configuration variables specific to simulation
+    x_pose = LaunchConfiguration('x_pose', default='1')
+    y_pose = LaunchConfiguration('y_pose', default='-2')
+    yaw_pose = LaunchConfiguration('yaw_pose', default='-1.5707')
+    tb3_model = LaunchConfiguration('tb3_model', default='turtlebot3_waffle')
+
+    # model_folder = 'turtlebot3_waffle'
+    urdf_path = os.path.join(
+        get_package_share_directory("final_project"),
+        "models",
+        tb3_model,
+        "model.sdf",
+    )
+    
+    # Declare the launch arguments
+    declare_model_cmd = DeclareLaunchArgument(
+        "tb3_model", default_value="turtlebot3_waffle", description="Model of the turtlebot3"
+    )
+    
+    declare_x_position_cmd = DeclareLaunchArgument(
+        'x_pose', default_value='1.0',
+        description='Specify namespace of the robot')
+
+    declare_y_position_cmd = DeclareLaunchArgument(
+        'y_pose', default_value='-2.0',
+        description='Specify namespace of the robot')
+    
+    declare_yaw_cmd = DeclareLaunchArgument(
+        'yaw_pose', default_value='1.5707',
+        description='Specify the yaw of the robot')
+
+    start_gazebo_ros_spawner_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'waffle',
+            '-file', urdf_path,
+            '-x', x_pose,
+            '-y', y_pose,
+            '-Y', yaw_pose,
+            '-z', '0.01'
+        ],
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_x_position_cmd)
+    ld.add_action(declare_y_position_cmd)
+    ld.add_action(declare_yaw_cmd)
+    ld.add_action(declare_model_cmd)
+
+    # Add any conditioned actions
+    ld.add_action(start_gazebo_ros_spawner_cmd)
+
+    return ld
